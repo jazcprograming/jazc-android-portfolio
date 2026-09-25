81bccaa1f123d34c00edba9dcae0f84aa0ef60bf
AbrahamZC <joseabrahamzapatacruz@gmail.com>
2026-09-24 20:29:08 -0500

    Poke API path or query - DTOs

--- app/src/main/java/com/jazc/portfolio/data/network/PokeApi.kt
+++ app/src/main/java/com/jazc/portfolio/data/network/PokeApi.kt
@@ -1,16 +1,57 @@
 package com.jazc.portfolio.data.network
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PokeApi {
+
+    @GET("pokemon")
+    suspend fun getPokemonList(@Query("limit") limit: Int,
+                               @Query("offset") offset: Int): PokemonListDTO
+
     @GET("pokemon/{nameOrId}")
-    suspend fun getPokemon(@Path("nameOrId") nameOrId: String): Pokemon
+    suspend fun getPokemon(@Path("nameOrId") nameOrId: String): PokemonDetailDTO
 }
 
-data class Pokemon(
+data class PokemonDetailDTO(
     val id: Int,
     val name: String,
     val height: Int,
     val weight: Int,
+    val types: List<TypeObjectDTO>,
+    val sprites: SpriteDTO
+)
+
+data class TypeObjectDTO(
+    val slot: Int,
+    val type: TypeDTO
+)
+
+data class TypeDTO(
+    val name: String
+)
+
+data class PokemonListDTO(
+    val count: Int,
+    val results: List<PokemonItemDTO>
+)
+
+data class SpriteDTO(
+    val other: PrincipalSpritesDTO
+)
+
+data class PrincipalSpritesDTO(
+    val home: SpritesLinkDTO
+)
+
+data class SpritesLinkDTO(
+    @SerializedName("front_default")
+    val frontDefault: String
+)
+
+data class PokemonItemDTO(
+    val name: String,
+    val url: String
 )
